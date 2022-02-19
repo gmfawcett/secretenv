b5e01bc67eef446f401b3b28a7b3734a70bfa563
Graham Fawcett <fawcett@uwindsor.ca>
2022-02-19 16:37:32 -0500

    clean up, add readme

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from distutils.core import setup
 
-setup(packages=["secretenv"])
+setup(
+    name="secretenv",
+    version="0.1",
+    description="playing with a secrets-in-repo idea",
+    author="gmfawcett",
+    url="https://github.com/gmfawcett/secretenv",
+    author_email="graham.fawcett@gmail.com",
+    license="MIT",
+    packages=["secretenv"],
+)
