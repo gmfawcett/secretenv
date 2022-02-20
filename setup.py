cbab30930ce5144e977b28843c53d33ba842c144
Graham Fawcett <fawcett@uwindsor.ca>
2022-02-19 19:51:16 -0500

    0.1.4: clean up example files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="secretenv",
-    version="0.1.3",
+    version="0.1.4",
     description="playing with a secrets-in-repo idea",
     long_description=long_description,
     long_description_content_type="text/markdown",
